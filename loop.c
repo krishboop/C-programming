9d32aca75c5e3f4a210190ea0d74a0deadbca11a
krishboop <krishpunia26@gmail.com>
2026-09-01 01:16:46 +0530

    while_loop

---
+++ loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=9999;
+    for(int i=0;i<n;i++){
+        printf("the value of i is %d \n",i);
+ }
+    return 0;
+}
---
+++ whileloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i=10;
+
+    while(i>3){
+        printf("the value of i is %d\n",i);
+        i--;
+    }
+return 0;
+}
+
